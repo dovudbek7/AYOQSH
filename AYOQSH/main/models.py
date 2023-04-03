d9b9cba94c5c16e7e4b03764f82053ba4b52b1d9
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-04-03 16:06:34 +0500

    add

--- AYOQSH/main/models.py
+++ AYOQSH/main/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 from django.apps import AppConfig
+from django.urls import reverse
 
 
 class ArticlesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
-# Create your models here.
+
+class City(models.Model):
+    city_name = models.CharField("City_name", max_length=50)
+    servise_name = models.CharField("Servise_name", max_length=50)
+    fuel1 = models.CharField('ai_80', max_length=50, default='')
+    fuel2 = models.CharField('ai_90', max_length=50, default='')
+    fuel3 = models.CharField('ai_91', max_length=50, default='')
+    fuel4 = models.CharField('ai_92', max_length=50, default='')
+    fuel5 = models.CharField('ai_98', max_length=50, default='')
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "City"
+
+    def __str__(self):
+        return self.city_name
+
+    def __str__(self):
+        return self.servise_name
+
+    # def get_absolute_url(self):
+    #     return reverse("filter", kwargs={"slug": self.url})
